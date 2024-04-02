8b20fee7cbd24715d16d8535b73645e66e36e0ea
Silvia Neves <siltatiana.sn@gmail.com>
2024-04-02 22:26:29 +0100

    Add express and app initialization in app.js, update dotenv configuration in server.js, and add port and app listening in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const app = require("./app");
+const path = require("path");
 
-dotenv.config();
+dotenv.config(
+  {
+    path: path.resolve(__dirname, "../.env"),
+  }
+);
 
 const db = process.env.DB_CONNECTION.replace(
   "<PASSWORD>",
@@ -12,3 +18,9 @@ mongoose
   .connect(db)
   .then(() => console.log("You are now connected to Mongo DB"))
   .catch((error) => console.log(error));
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
